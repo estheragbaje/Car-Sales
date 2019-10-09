817043157d74b3a6a1c3849405da014f91ed2686
Folasade Agbaje <folasadeagbaje@gmail.com>
2019-10-09 18:37:21 +0100

    Created ability to add and remove feature from car

--- src/App.js
+++ src/App.js
@@ -1,29 +1,31 @@
 import React from "react";
-import { combineReducers } from "redux";
-
+import { connect } from "react-redux";
 import Header from "./components/Header";
 import AddedFeatures from "./components/AddedFeatures";
 import AdditionalFeatures from "./components/AdditionalFeatures";
 import Total from "./components/Total";
+import * as actionCreators from "./state/actionCreators";
 
-const App = () => {
-  const state = {
-    additionalPrice: 0,
-    car: {
-      price: 26395,
-      name: "2019 Ford Mustang",
-      image:
-        "https://cdn.motor1.com/images/mgl/0AN2V/s1/2019-ford-mustang-bullitt.jpg",
-      features: []
-    },
-    store: [
-      { id: 1, name: "V-6 engine", price: 1500 },
-      { id: 2, name: "Racing detail package", price: 1500 },
-      { id: 3, name: "Premium sound system", price: 500 },
-      { id: 4, name: "Rear spoiler", price: 250 }
-    ]
-  };
+// export const state = {
+//   additionalPrice: 0,
+//   car: {
+//     price: 26395,
+//     name: "2019 Ford Mustang",
+//     image:
+//       "https://cdn.motor1.com/images/mgl/0AN2V/s1/2019-ford-mustang-bullitt.jpg",
+//     features: []
+//   },
+//   store: [
+//     { id: 1, name: "V-6 engine", price: 1500 },
+//     { id: 2, name: "Racing detail package", price: 1500 },
+//     { id: 3, name: "Premium sound system", price: 500 },
+//     { id: 4, name: "Rear spoiler", price: 250 }
+//   ]
+// };
 
+const App = props => {
+  const { store } = props;
+  // const state = testState;
   const removeFeature = item => {
     // dispatch an action here to remove an item
   };
@@ -35,15 +37,18 @@ const App = () => {
   return (
     <div className="boxes">
       <div className="box">
-        <Header car={state.car} />
-        <AddedFeatures car={state.car} />
+        <Header car={store.car} />
+        <AddedFeatures car={store.car} />
       </div>
       <div className="box">
-        <AdditionalFeatures store={state.store} />
-        <Total car={state.car} additionalPrice={state.additionalPrice} />
+        <AdditionalFeatures store={store.store} />
+        <Total car={store.car} additionalPrice={store.additionalPrice} />
       </div>
     </div>
   );
 };
 
-export default App;
+export default connect(
+  state => state,
+  actionCreators
+)(App);
--- src/components/AdditionalFeature.js
+++ src/components/AdditionalFeature.js
@@ -1,13 +1,27 @@
-import React from 'react';
+import React from "react";
+import { connect } from "react-redux";
+import * as actionCreators from "../state/actionCreators";
 
-const AdditionalFeature = props => {
+export const AdditionalFeature = props => {
+  const { feature, addFeature } = props;
+
   return (
     <li>
       {/* Add an onClick that will let you add a feature to your car */}
-      <button className="button">Add</button>
+      <button
+        className="button"
+        onClick={() => {
+          addFeature(feature);
+        }}
+      >
+        Add
+      </button>
       {props.feature.name} (+{props.feature.price})
     </li>
   );
 };
 
-export default AdditionalFeature;
+export default connect(
+  state => state,
+  actionCreators
+)(AdditionalFeature);
